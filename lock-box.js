6e4c3b4c745345a9398188be07302c9665bb1895
orlando.tyrell <orlandotyrell@gmail.com>
2022-06-02 17:16:21 -0400

    catching up

---
+++ lock-box.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')({sigint: true});
+    let access = prompt ("You may enter: ");
+
+    let pinCode = ("1234");
+
+    if (pinCode === "1234")
+    {
+        console.log("Success");
+    } 
+    else (pinCode != "1234");
+    console.log("Failure");
---
+++ rock-paper-scissors.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')({sigint: true});
+    let game = prompt ("You may enter: ");
+
+    let Player1 = ("rock, paper, scissors");
+    if (Player1 === ("rock"));
+    if (Player2 === ("paper"));
+   { console.log ("Player1 wins"); }
